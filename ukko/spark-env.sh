af1ec1e16163801b773e6fd7cef7f1985629d36b
Peter Goetsch <pgoetsch@dminc.com>
2018-01-19 10:13:34 +0200

    Add env

---
+++ ukko/spark-env.sh
@@ -0,0 +1,6 @@
+export JAVA_HOME=/cs/home/goetsch/dev/java/jdk1.8.0_131
+export HADOOP_HOME=/cs/home/goetsch/dev/hadoop-2.8.1
+export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
+export SPARK_HOME=/cs/home/goetsch/dev/spark-2.1.0-bin-hadoop2.7
+
+export PATH=$PATH:$HADOOP_HOME/bin
