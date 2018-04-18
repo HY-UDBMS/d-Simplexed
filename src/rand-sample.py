700c4592b78e581c33b67b3525ef498e64d0410a
Peter Goetsch <pgoetsch@dminc.com>
2018-04-19 00:21:06 +0300

    Modify to support passing sample number and output format via command-line

--- src/rand-sample.py
+++ src/rand-sample.py
@@ -1,3 +1,4 @@
+import sys
 import random
 
 def is_divisible_by(nums, n):
@@ -6,7 +7,7 @@ def is_divisible_by(nums, n):
 			return True
 
 # pick n random pairs of (vmem, vcores) execution configurations within the bounds of xmin, xmax, ymin and ymax
-def get_rand_points(xmin, xmax, ymin, ymax, n):
+def get_rand_points(xmin, xmax, ymin, ymax, n, out_format):
 	rand_points = []
 
 	for i in range(n):
@@ -29,8 +30,20 @@ def get_rand_points(xmin, xmax, ymin, ymax, n):
 				vcores_per_exec = vcores / divisor
 				vmem_per_exec = vmem * 1000 / divisor 
 
-				print(str(vcores) + "v" + str(vmem) + "g --> " + str(executors) + " execs, " + str(vcores_per_exec) + " cores/exec, " + str(vmem_per_exec) + "mb vmem/exec");
+				if out_format == "pretty":
+					print(str(vcores) + "v" + str(vmem) + "g --> " + str(executors) + " execs, " + str(vcores_per_exec) + " cores/exec, " + str(vmem_per_exec) + "mb vmem/exec");
+				elif out_format == "hibench":
+					print(str(executors) + "\t" + str(vcores_per_exec) + "\t" + str(vmem_per_exec) + "mb");
+				else:
+					print "invalid output format: " + out_format
 				break
 
+if len(sys.argv) < 3:
+	print "Usage: rand-sample.py sample-num output=hibench|output=pretty" 
+	sys.exit()
+
+n = int(sys.argv[1])
+out_format = sys.argv[2].replace("output=", "")
+
 # select from 2-56gb vmem and 2-15 vcores (need to remember to leave some resources for the host)
-get_rand_points(2, 56, 2, 15, 10)
+get_rand_points(2, 56, 2, 15, n, out_format)
