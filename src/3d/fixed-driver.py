abb866cb2148840ec8ca9bc29b6af7d6b642112e
Peter Goetsch <pgoetsch@dminc.com>
2018-05-21 15:26:29 +0300

    Add gridding tests in

--- src/3d/fixed-driver.py
+++ src/3d/fixed-driver.py
@@ -41,27 +41,35 @@ print "Size of historical data pool: " + str(input_len)
 
 # TODO: make this dynamic
 # len(f1) must equal len(f2)
-f1 = [10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50]
-f2 = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
-feature_space = [f1,f2]
-#f1 = [10, 20, 30, 40, 50]
-#f2 = [5, 10, 15, 20, 25]
-
+f1_range = [10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50]
+f2_range = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
+#f1_range = np.arange(40,242,2).tolist()
+#f2_range = np.arange(60,161,1).tolist()
+feature_space = [f1_range,f2_range]
 
 # < 4 (8 total points) and you run into problems forming the triangulation
-seed_lhs_count = 4 
-seed_samples = sampler.seed_sample([f1, f2], seed_lhs_count);
+#seed_lhs_count = 4 
+#seed_samples = sampler.seed_sample([f1_range, f2_range], seed_lhs_count);
+seed_samples = []
+
+grid_samples = sampler.get_gridding_samples(feature_space, [40,20,10,5,4,2,1])
+
+print "grid_samples=" + str(grid_samples)
+
+for i in range(8):
+	seed_samples.append(grid_samples.pop(0))
+
 seed_samples = [[[f1,f2], lookup_runtime([f1,f2], hist_data)] for [f1, f2] in seed_samples]
 seed_count = len(seed_samples)
 
 print "Number of seed samples from sampler: " + str(seed_count)
 print "Seed samples from sampler: " + str(seed_samples)
 print "Adding in boundary samples..."
-boundary_samples = [[10,5],[10,25],[50,5],[50,25]]
-for boundary_sample in boundary_samples:
-	if not contains_sample(seed_samples, boundary_sample):
-		print "add boundary " + str(boundary_sample)
-		seed_samples.append([boundary_sample, lookup_runtime(boundary_sample, hist_data)]);
+#boundary_samples = [[f1_range[0],f2_range[0]],[f1_range[0],f2_range[-1]],[f1_range[-1],f2_range[0]],[f1_range[-1],f2_range[-1]]]
+#for boundary_sample in boundary_samples:
+#	if not contains_sample(seed_samples, boundary_sample):
+#		print "add boundary " + str(boundary_sample)
+#		seed_samples.append([boundary_sample, lookup_runtime(boundary_sample, hist_data)]);
 
 print "finished seed samples: " + str(seed_samples)
 print "len(seed_samples) after everything: " + str(len(seed_samples))
@@ -82,15 +90,10 @@ for seed in seed_samples:
 random.shuffle(hist_data)
 test_set = []
 while len(test_set) < test_set_count:
-	next_tester = hist_data[-1]
-	# if we happen to pick at random a seed point, exclude it
-	#already_in_seeds = False
-	#for seed_sample in seed_samples:
-#		if seed_sample[0] == next_tester:
-#			already_in_seeds = True
-	
-#	if not already_in_seeds:
-	test_set.append(hist_data.pop())
+	next_for_test_set = hist_data[0]
+	# if the point we want to save for the test set is in seed_samples, don't add it
+	if next_for_test_set not in seed_samples:
+		test_set.append(hist_data.pop(0))
 
 print "len(test_set) = " + str(len(test_set))
 print "len(hist_data) = " + str(len(hist_data))
@@ -98,9 +101,6 @@ print "Size of historical data pool: " + str(len(hist_data))
 print "Size of seed_samples: " + str(len(seed_samples))
 print "hist_data before subtracting seed_samples" + str(hist_data)
 print "seed_samples here: " + str(seed_samples)
-
-
-# TODO: make sure these line-up, test_set should be exactly 20% of hist_data + seed_samples
 print "len(hist_data) " + str(len(hist_data))
 print "len(test_set) " + str(len(test_set))
 print "len(seed_samples) " + str(len(seed_samples))
@@ -112,10 +112,6 @@ assert (len(hist_data) + len(test_set) + len(seed_samples)) == input_len
 # Build the model with the seed points
 #######################################
 
-
-# build initial model with seed_samples
-dt = DelaunayModel(seed_samples)
-dt.construct_model()
 
 # Instanciate a Gaussian Process model
 kernel = C(1.0, (1e-3, 1e3)) * RBF(1, (1e-5, 1e5))
@@ -139,7 +135,7 @@ for idx,runtime_val in enumerate(gp_testset_Y):
 	pct_err = round(abs((actual_runtime - est_runtime) / actual_runtime), 5)
 	total_err += pct_err
 	sample_count += 1
-	#print "[GP]{}: Predicted={} Actual={}, MPE={}%".format(gp_testset_X[idx], est_runtime, actual_runtime, pct_err * 100)
+	#print "[GP]{}: Predicted={} Actual={}, MAPE={}%".format(gp_testset_X[idx], est_runtime, actual_runtime, pct_err * 100)
 
 mape_gp = round(total_err/sample_count*100,2)
 
@@ -162,7 +158,11 @@ for idx,runtime_val in enumerate(gp_testset_Y):
 
 mape_lr = round(total_err/sample_count*100,2)
 
-# determine overall MPE of model with just seed samples
+# build initial model with seed_samples
+dt = DelaunayModel(seed_samples)
+dt.construct_model()
+
+# determine overall MAPE of model with just seed samples
 total_err = 0 
 sample_count = 0
 for [a,b],c in test_set:
@@ -172,12 +172,10 @@ for [a,b],c in test_set:
 	pct_err = round(abs((actual_runtime - est_runtime) / actual_runtime), 5)
 	total_err += pct_err
 	sample_count += 1
-	print "{}: Predicted={} Actual={}, MPE={}%".format([a,b], est_runtime, actual_runtime, pct_err * 100)
+	print "{}: Predicted={} Actual={}, MAPE={}%".format([a,b], est_runtime, actual_runtime, pct_err * 100)
 
 mape_dt = round(total_err/sample_count*100,2)
-#print "[Reporting-DT] {}\t{}\t{}".format(len(seed_samples), sample_count, mape_dt)
-#print "[Reporting-GP] {}\t{}\t{}".format(len(seed_samples), sample_count, mape_gp)
-#print "[Reporting-LR] {}\t{}\t{}".format(len(seed_samples), sample_count, mape_lr)
+
 print "[Reporting] {}\t{}\t{}\t{}".format(len(seed_samples), mape_dt, mape_gp, mape_lr)
 
 # then add 1-by-1 new points via adaptive sampling, re-building the model and noting the error
@@ -190,48 +188,54 @@ model_samples = list(seed_samples)
 while len(hist_data) > 3:
 	# grab next sample and remove from hist_data
 	print "len(hist_data) BEFORE " + str(len(hist_data))
-	feature_space = [[10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50],[5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]]
-	next_sample = sampler.next_adaptive_sample(model_samples, hist_data, feature_space)
-	print "len(hist_data) AFTER " + str(len(hist_data))
+	#feature_space = [[10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50],[5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]]
+	for i in range(5):
+		found_sample = False
+		while not found_sample:
+			next_sample = grid_samples.pop(0)
+			if next_sample not in [x[0] for x in test_set]:
+				# if next_sample in test set, ignore it
+				found_sample = True
 
-	print "next_sample=" + str(next_sample)
-	model_samples.append(next_sample)
+		print "next_sample=" + str(next_sample)
+		print "next_sample runtime=" + str(lookup_runtime(next_sample, hist_data))
+		#next_sample = sampler.next_random_sample(model_samples, hist_data, feature_space) 
+		#next_sample = sampler.next_adaptive_sample(model_samples, hist_data, feature_space)
+		model_samples.append([next_sample,lookup_runtime(next_sample, hist_data)])
 
 	dt = DelaunayModel(model_samples)
 	dt.construct_model()
 
 	# Instanciate a Gaussian Process model
-	kernel = C(1.0, (1e-3, 1e3)) * RBF(1, (1e-5, 1e5))
-	gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=3)
+	#kernel = C(1.0, (1e-3, 1e3)) * RBF(1, (1e-5, 1e5))
+	#gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=3)
 
-	gp_training_X = [[f1,f2] for [[f1,f2],runtime] in model_samples]
-	gp_training_Y = [[runtime] for [[f1,f2],runtime] in model_samples]
+	#gp_training_X = [[f1,f2] for [[f1,f2],runtime] in model_samples]
+	#gp_training_Y = [[runtime] for [[f1,f2],runtime] in model_samples]
 
-	gp_testset_X = [[f1,f2] for [[f1,f2],runtime] in test_set]
-	gp_testset_Y = [[runtime] for [[f1,f2],runtime] in test_set]
+	#gp_testset_X = [[f1,f2] for [[f1,f2],runtime] in test_set]
+	#gp_testset_Y = [[runtime] for [[f1,f2],runtime] in test_set]
 
 	# Fit to data using Maximum Likelihood Estimation of the parameters
-	gp.fit(gp_training_X, gp_training_Y)
+	#gp.fit(gp_training_X, gp_training_Y)
 
-	y_pred, sigma = gp.predict(gp_testset_X, return_std=True)
+	#y_pred, sigma = gp.predict(gp_testset_X, return_std=True)
 
-	total_err = 0 
-	sample_count = 0
-	for idx,runtime_val in enumerate(gp_testset_Y):
-		est_runtime = y_pred[idx]
-		actual_runtime = runtime_val
-		pct_err = round(abs((actual_runtime - est_runtime) / actual_runtime), 5)
-		total_err += pct_err
-		sample_count += 1
+	#total_err = 0 
+	#sample_count = 0
+	#for idx,runtime_val in enumerate(gp_testset_Y):
+	#	est_runtime = y_pred[idx]
+	#	actual_runtime = runtime_val
+	#	pct_err = round(abs((actual_runtime - est_runtime) / actual_runtime), 5)
+	#	total_err += pct_err
+	#	sample_count += 1
 
-	mape_gp = round(total_err/sample_count*100,2)
-	
+	#mape_gp = round(total_err/sample_count*100,2)
+	mape_gp = 0
 	# Use basic multivariate linear regression (X,Y)-->Z
 	lm = linear_model.LinearRegression()
 	model = lm.fit(gp_training_X, gp_training_Y)
 
-	#testX = [(25, x) for x in np.linspace(1, 70, 1000)]
-	#print(testX)
 	# # Make the prediction on the meshed x-axis (ask for MSE as well)
 	y_pred = lm.predict(gp_testset_X)
 	total_err = 0 
@@ -253,11 +257,8 @@ while len(hist_data) > 3:
 		pct_err = round(abs((actual_runtime - est_runtime) / actual_runtime), 5)
 		total_err += pct_err
 		sample_count += 1
-		print "{}: Predicted={} Actual={}, MPE={}%".format([a,b], est_runtime, actual_runtime, pct_err * 100)
+		print "{}: Predicted={} Actual={}, MAPE={}%".format([a,b], est_runtime, actual_runtime, pct_err * 100)
 
 	mape_dt = round(total_err/sample_count*100,2)
 	
-	#print "[Reporting-DT] {}\t{}\t{}".format(len(model_samples), sample_count, mape_dt)
-	#print "[Reporting-GP] {}\t{}\t{}".format(len(model_samples), sample_count, mape_gp)
-	#print "[Reporting-LR] {}\t{}\t{}".format(len(model_samples), sample_count, mape_lr)
 	print "[Reporting] {}\t{}\t{}\t{}".format(len(model_samples), mape_dt, mape_gp, mape_lr)
